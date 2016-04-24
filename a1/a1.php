a7e54ebff57e08b21929668f093aacbc5e8e2788
Daniel Bischoff <danielbischoff@hotmail.com>
2016-04-24 21:09:59 +0200

    Etwas aufgeräumt

--- a1/a1.php
+++ a1/a1.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <script type="text/javascript" src="js/jquery-2.2.3.min.js"></script>
+  <script type="text/javascript" src="lib/jquery-2.2.3.min.js"></script>
   <script type="text/javascript" src="js/main.js"></script>
   <link rel="stylesheet" href="css/main.css">
   <title>Aufgabe 1</title>
@@ -17,17 +17,3 @@
   </div>
 </body>
 </html>
-
-<!--<script type="text/javascript" src="js/Ajax.js"></script>-->
-<!--<script type="text/javascript" src="js/Writer.js"></script>-->
-<!--<script type="text/javascript">-->
-<!--    write2console("Das ist das Consolen-Fenster");-->
-<!--    console.log("das ist eine Ausgabe in die Console des Browsers");-->
-<!--    var URL = "http://localhost/Ajax-Console/getDate.php";-->
-<!--    var ajaxCom= new Ajax(URL,receive);-->
-<!--    // expected components (checked in receive())-->
-<!--    receivedObj= {"date": 0};-->
-<!--    ajaxCom.send({"msg": "Heute ist "});-->
-<!--    write2console(receivedObj.date);-->
-<!--    ajaxCom.disconnect();-->
-<!--</script>-->
