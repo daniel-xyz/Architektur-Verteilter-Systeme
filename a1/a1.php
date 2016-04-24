7f65f8a684d827263422cc9b50fca81ff03e1736
Daniel Bischoff <danielbischoff@hotmail.com>
2016-04-24 20:27:09 +0200

    jQuery dem Projekt hinzugefügt.

--- a1/a1.php
+++ a1/a1.php
@@ -2,6 +2,8 @@
 <html>
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <script type="text/javascript" src="js/jquery-2.2.3.min.js"></script>
+  <script type="text/javascript" src="js/main.js"></script>
   <link rel="stylesheet" href="css/main.css">
   <title>Aufgabe 1</title>
 </head>
@@ -9,8 +11,8 @@
   <div class="main-container">
 
     <div class="button-container">
-      <input type="button" value="Nachricht erzeugen"/>
-      <input type="button" value="Nachrichten anzeigen"/>
+      <input type="button" onclick="sendMessage()" value="Nachricht erzeugen"/>
+      <input type="button" onclick="showMessages()" value="Nachrichten anzeigen"/>
     </div>
 
     <div class="message-container"></div>
---
+++ a1/js/main.js
@@ -0,0 +1,7 @@
+function sendMessage() {
+
+}
+
+function showMessages() {
+
+}
