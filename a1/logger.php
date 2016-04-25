b0c853522fd3280c339a36a81e5a54af352676d3
Daniel Bischoff <danielbischoff@hotmail.com>
2016-04-25 16:22:30 +0200

    Logger loggt in messages.txt

--- a1/logger.php
+++ a1/logger.php
@@ -2,10 +2,22 @@
 
 class Logger {
 
+  static $fileName = 'messages.txt';
   static $messages = array();
 
   public static function log($message) {
-    array_push(self::$messages, $message);
+    if(file_exists(self::$fileName)) {
+      $data = file_get_contents(self::$fileName);
+
+      if(strlen($data) > 0) {
+        self::$messages = unserialize($data);
+      }
+    }
+
+    self::$messages[] = $message;
+
+    file_put_contents(self::$fileName, serialize(self::$messages));
+
     print_r(self::$messages); // Nur zum Debuggen, um das Array im Browser in der Response zu sehen.
   }
 }
