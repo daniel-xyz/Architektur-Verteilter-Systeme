1ff4838ad3e37935701db6375bb13fdca1c94e94
Daniel Bischoff <danielbischoff@hotmail.com>
2016-05-18 15:06:15 +0200

    extended bash script

--- a2/prepare-server-a2.sh
+++ a2/prepare-server-a2.sh
@@ -2,5 +2,6 @@
 sudo sh -c 'cd /opt/lampp/htdocs/Architektur-Verteilter-Systeme && git fetch origin master && git reset --hard FETCH_HEAD && git clean -df' &&
 cd /opt/lampp/htdocs/Architektur-Verteilter-Systeme/a2 &&
 sudo chmod 777 messages.txt &&
-sh /opt/lampp/lampp startapache
-curl http://localhost/Architektur-Verteilter-Systeme/a1/notifyRegistry.php
+sh /opt/lampp/lampp startapache &&
+curl http://localhost/Architektur-Verteilter-Systeme/a1/notifyRegistry.php &&
+read -p "Press Enter to continue > "
