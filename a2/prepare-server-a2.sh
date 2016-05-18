5b6f884694121a3a2f7759ae9bae46593bf9a901
Daniel Bischoff <danielbischoff@hotmail.com>
2016-05-18 14:56:46 +0200

    fixed typo

--- a2/prepare-server-a2.sh
+++ a2/prepare-server-a2.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 sudo sh -c 'cd /opt/lampp/htdocs/Architektur-Verteilter-Systeme && git fetch origin master && git reset --hard FETCH_HEAD && git clean -df' &&
-cd /opt/lampp/htdocs/Architektur-Verteilter-Systeme/a1 &&
+cd /opt/lampp/htdocs/Architektur-Verteilter-Systeme/a2 &&
 sudo chmod 777 messages.txt &&
 sh /opt/lampp/lampp startapache
