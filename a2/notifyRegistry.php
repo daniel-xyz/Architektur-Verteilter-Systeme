93bcdaed29064a59f892f7e74551f4c889b359d0
Daniel Bischoff <danielbischoff@hotmail.com>
2016-05-20 19:32:01 +0200

    test if server receives ip list

--- a2/notifyRegistry.php
+++ a2/notifyRegistry.php
@@ -12,7 +12,7 @@ if(!empty($_REQUEST['name'] && !empty($_REQUEST['ip']))) {
   $registryServer = $_REQUEST['ip'];
 }
 
-$request = new HTTP_Request2($registryServer . '/Architektur-Verteilter-Systeme/a2/api/registry.php', HTTP_Request2::METHOD_GET);
+$request = new HTTP_Request2('http://' . $registryServer . '/Architektur-Verteilter-Systeme/a2/api/registry.php', HTTP_Request2::METHOD_GET);
 
 $url = $request->getUrl();
 $url->setQueryVariable('name', $serverName);
@@ -21,9 +21,9 @@ try {
   $response = $request->send();
 
   if (200 == $response->getStatus()) {
-    //echo $response->getBody();
+    $ipList = $response->getBody();
     var_dump(http_response_code(200));
-    echo('Registry-Server hat Request erhalten.');
+    echo('Registry-Server hat Request erhalten. und antwortete mit IP-Liste: ' . $ipList);
   } else {
     echo 'Unerwarteter HTTP-Status: ' . $response->getStatus() . ' ' . $response->getReasonPhrase();
     var_dump(http_response_code(404));
