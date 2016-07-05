f742199ed4822af975c4a8099b5983488e8dab55
Daniel Bischoff <danielbischoff@hotmail.com>
2016-07-05 17:10:22 +0200

    fixed file path

--- a3/class/IPListHandler.class.php
+++ a3/class/IPListHandler.class.php
@@ -5,16 +5,16 @@ require_once('FileHandler.class.php');
 class IPListHandler {
 
   private $fileHandler;
-  private $fileName = 'persistence/iplist.txt';
+  private $fileName = '../persistence/iplist.txt';
 
   function __construct() {
     $this->fileHandler = new FileHandler();
   }
 
   public function update($ipList) {
-    $ipList = $this->fileHandler->deserialize($this->fileName);
-    $ipList['all'] = $ipList;
-    $this->fileHandler->serialize($this->fileName, $ipList);
+    $deserialized = $this->fileHandler->deserialize($this->fileName);
+    $deserialized['all'] = $ipList;
+    $this->fileHandler->serialize($this->fileName, $deserialized);
   }
 
   public function getList() {
