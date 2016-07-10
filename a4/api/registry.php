189ce27499b213bf0d6cfc1a975780dd593de4e5
Daniel Bischoff <danielbischoff@hotmail.com>
2016-07-10 15:26:47 +0200

    optimized registry api

--- a4/api/registry.php
+++ a4/api/registry.php
@@ -10,14 +10,33 @@ if (!empty($_REQUEST['name'])) {
 
   if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
     $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-    addToIpList($name, $ip);
     sendInformationToNewServer($name, $ip);
 } elseif(!empty($_SERVER['REMOTE_ADDR'])) {
     $ip = $_SERVER['REMOTE_ADDR'];
-    addToIpList($name, $ip);
     sendInformationToNewServer($name, $ip);
   } else {
     user_error("IP konnte nicht ermittelt werden.");
+  }
+}
+
+function sendInformationToNewServer($name, $ip) {
+  $request = new HTTP_Request2('http://' . $ip . '/Architektur-Verteilter-Systeme/a4/notifyRegistry.php', HTTP_Request2::METHOD_GET);
+
+  $url = $request->getUrl();
+  $url->setQueryVariable('yourname', $name);
+  $url->setQueryVariable('yourip', $ip);
+
+  try {
+    $response = $request->send();
+
+    if (200 == $response->getStatus()) {
+      addToIpList($name, $ip);
+      triggerNeighborNotifications();
+    } else {
+      echo 'Unerwarteter HTTP-Status vom Registry-Server: ' . $response->getStatus() . '. ' . $response->getReasonPhrase() . ' ';
+    }
+  } catch (HTTP_Request2_Exception $e) {
+    error_log($e->getMessage());
   }
 }
 
@@ -33,26 +52,6 @@ function addToIpList($name, $ip) {
   error_log('Server in der Registry registriert: ' . $ipList[$ip]['name'] . ' ' . $ipList[$ip]['ip']);
 
   $ipListHandler->update($ipList);
-}
-
-function sendInformationToNewServer($name, $ip) {
-  $request = new HTTP_Request2('http://' . $ip . '/Architektur-Verteilter-Systeme/a4/notifyRegistry.php', HTTP_Request2::METHOD_GET);
-
-  $url = $request->getUrl();
-  $url->setQueryVariable('yourname', $name);
-  $url->setQueryVariable('yourip', $ip);
-
-  try {
-    $response = $request->send();
-
-    if (200 == $response->getStatus()) {
-      triggerNeighborNotifications();
-    } else {
-      echo 'Unerwarteter HTTP-Status vom Registry-Server: ' . $response->getStatus() . '. ' . $response->getReasonPhrase() . ' ';
-    }
-  } catch (HTTP_Request2_Exception $e) {
-    error_log($e->getMessage());
-  }
 }
 
 function triggerNeighborNotifications() {
