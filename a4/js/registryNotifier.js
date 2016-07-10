919e6ead972d3f1b37c151269dfc9bde5eba3f28
Daniel Bischoff <danielbischoff@hotmail.com>
2016-07-10 17:25:52 +0200

    optimized registry api

--- a4/js/registryNotifier.js
+++ a4/js/registryNotifier.js
@@ -2,6 +2,7 @@ function notifyRegistry() {
   var serverName = $('#server-name').val();
   var registry = $('#ip').val() || "";
   var newIP = $('#invited-ip').val() || "";
+  var kickIP = $('#kick-ip').val() || "";
   var params = {};
 
   resetStatusMessages();
@@ -11,16 +12,20 @@ function notifyRegistry() {
       name: serverName,
       registryip: registry
     }
-  } else {
+  } else if (newIP !== "") {
     params = {
       name: serverName,
       newip: newIP
+    }
+  } else if (kickIP !== "") {
+    params = {
+      kickip: kickIP
     }
   }
 
   $.get("notifyRegistry.php", params)
     .success(function() {
-      msg('Server wurde erfolgreich angemeldet. Viel Spaß!');
+      msg('Registry wurde erfolgreich aktualisiert. Viel Spaß!');
     })
     .fail(function() {
       error('Es ist leider etwas schief gelaufen.');
